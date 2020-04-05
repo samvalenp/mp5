d98ce128380a515b99714e86c038a9689dc706bf
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-04-05 21:38:31 +0200

    parte a 15

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -26,10 +26,10 @@ lines = sc.textFile(sys.argv[3],1)
 #START: tokenize, lowercase, stopwords
 
 # tokenize
-words1 = lines.flatMap(lambda line: re.split(r'[_]', line))
+words1 = lines.flatMap(lambda line: re.split(r'[\t,;\.\?!-:@\[\]\(\)\{\}\_\*/]', line))
 first = words1.take(100)
 firstrdd = sc.parallelize(first)
-firstrdd.saveAsTextFile('hola1')
+firstrdd.saveAsTextFile('hola2')
 
 # lowercase
 words2 = words1.map(lambda word: word.lower())
