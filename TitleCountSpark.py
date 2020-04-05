a90cd694bff88d387cdbe8712645d5447104e8ef
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-04-05 20:13:00 +0200

    parte a 1

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -33,7 +33,7 @@ words3 = words2.filter(lambda word: word not in stopWords)
 # count the occurrence of each word
 wordCounts = words3.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a+b)
 # top 10
-top = wordCounts.takeOrdered(10, lambda (word, count): -1*count)
+top = wordCounts.takeOrdered(10, lambda (word,count): -1*count)
 
 #END
 
