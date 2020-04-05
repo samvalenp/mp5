91f5f3624079508aea801e58a7ee95ee97a679bc
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-04-05 20:11:13 +0200

    parte a

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -8,11 +8,13 @@ from pyspark import SparkConf, SparkContext
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
+stopWords = set()
 with open(stopWordsPath) as f:
-	#TODO
+	for linea in f:
+		stopwords.add(linea)
 
 with open(delimitersPath) as f:
-    #TODO
+	delimiters = f.readLine()
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
@@ -20,11 +22,28 @@ sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[3],1)
 
-#TODO
+#START: tokenize, lowercase, stopwords
 
-outputFile = open(sys.argv[4],"w")
+# tokenize
+words1 = lines.flatMap(lambda line: line.split(" "))
+# lowercase
+words2 = words1.map(lambda word: word.lower())
+# stopwords
+words3 = words2.filter(lambda word: word not in stopWords)
+# count the occurrence of each word
+wordCounts = words3.map(lambda word: (word, 1)).reduceByKey(lambda a,b: a+b)
+# top 10
+top = wordCounts.takeOrdered(10, lambda (word, count): -1*count)
 
-#TODO
+#END
+
+# outputFile = open(sys.argv[4],"w")
+
+#START
+topOut = top.sortByKey().map(lambda (word, count): word+ " " + str(count))
+topOut.saveAsTextFile(sys.argv[4])
+
+#END
 #write results to output file. Foramt for each line: (line +"\n")
 
 sc.stop()
