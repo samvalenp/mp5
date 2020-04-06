4b0dad548f63f8492d372b194ddb76a1a327b2c5
Samuel Valenzuela <Sam@Samuels-MacBook-Pro.local>
2020-04-06 13:24:46 +0200

    parte a 28

--- TitleCountSpark.py
+++ TitleCountSpark.py
@@ -16,7 +16,7 @@ with open(stopWordsPath) as f:
 		stopWords.add(linea.strip())
 
 with open(delimitersPath) as f:
-	delimiters = f.read().s
+	delimiters = f.read()
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
